1d4a4e73753ef8ab7ae1abc5f3d5c1936569cca1
dimasdwi123 <dimasdwi455@gmail.com>
2023-10-08 17:12:38 +0700

    menambahkan .env

--- app/model/connection.js
+++ app/model/connection.js
@@ -2,10 +2,10 @@ const sequelize = require('sequelize')
 const mysql = require('mysql2')
 require('dotenv').config()
 
-const connection = new sequelize.Sequelize('mydb', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false,
+const connection = new sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
+    host: process.env.MYSQLHOST ,
+    port: process.env.MYSQLPORT,
+    dialect : 'mysql',
   })
 
 module.exports = connection
